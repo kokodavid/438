e4a730f5ba956127c5035c991d073c9ac5d11e08
KokoDavid <kingkgdave@gmail.com>
2020-04-16 14:39:42 +0300

    Google Maps Code

---
+++ app/src/main/java/com/koko/a438/MainActivity.java
@@ -0,0 +1,34 @@
+package com.koko.a438;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mapbutton) Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent i = new Intent(MainActivity.this, Location.class);
+               startActivity(i);
+           }
+       });
+
+    }
+}
