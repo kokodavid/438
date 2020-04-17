a9022a27373654b502147031f4e2d6569d654076
KokoDavid <kingkgdave@gmail.com>
2020-04-17 12:35:04 +0300

    initial commit for news feature

--- app/src/main/java/com/koko/a438/MainActivity.java
+++ app/src/main/java/com/koko/a438/MainActivity.java
@@ -3,6 +3,7 @@ package com.koko.a438;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
---
+++ app/src/main/java/com/koko/a438/newsFeature/NewsActivity.java
@@ -0,0 +1,16 @@
+package com.koko.a438.newsFeature;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.koko.a438.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
